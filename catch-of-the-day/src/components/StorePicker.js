2bea0f5e9251ddd006dc0cd6e8b2447a1d950099
rojasdali <rojasdali@gmail.com>
2019-01-23 10:34:19 -0500

    add heading

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,12 +4,11 @@ import React from 'react';
 class StorePicker extends React.Component {
     render(){
         return (
-            <React.Fragment> 
-                <p>Fish!</p>   
                 <form action="" className="store-selector">
                     <h2>Please Enter A Store</h2>
+                    <input type="text" required placeholder="Store Name"/>
+                    <button type ="submit">Visit Store -></button>
                 </form>
-            </React.Fragment>
         )
     }
 }
