256ed6b857685f6ff52db39a4913006501a4aae2
rojasdali <rojasdali@gmail.com>
2019-01-23 10:05:46 -0500

    basics

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+    render(){
+        return (
+            <React.Fragment> 
+                <p>Fish!</p>   
+                <form action="" className="store-selector">
+                    <h2>Please Enter A Store</h2>
+                </form>
+            </React.Fragment>
+        )
+    }
+}
+
+export default StorePicker;
