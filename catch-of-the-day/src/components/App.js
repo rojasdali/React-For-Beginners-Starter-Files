d797ea59079bc91a0bbbfabf9f39eb18c3cbf4e4
rojasdali <rojasdali@gmail.com>
2019-01-23 14:42:34 -0500

    Add fish to state, set state updates, form reset

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,24 @@ import Inventory from './Inventory'
 import Order from './Order'
 
 class App extends React.Component {
+
+    state = {
+            fishes: {},
+            order: {}
+    }
+
+    addFish = fish => {
+        // take a copy of the existing state
+        const fishes = {...this.state.fishes };
+        // add a new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // this.state.fishes.push(fish)
+        // this.state.fishes.fish1 = fish;
+        this.setState({
+            fishes : fishes
+        })
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +29,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                     <Order />
-                    <Inventory />
+                    <Inventory addFish={this.addFish}/>
             </div>
         )
     }
